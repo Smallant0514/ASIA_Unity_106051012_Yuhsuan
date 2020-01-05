be512fbb9ee2a8578f341e732162ec596b27c275
Smallant0514 <55477183+Smallant0514@users.noreply.github.com>
2020-01-05 21:45:38 +0800

    完成發布網頁

---
+++ Unity_106051012_Yuhsuan/Assets/Scripts/Soldier.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class Soldier : MonoBehaviour
+{
+    public int speed = 10;
+    public float turn = 20.5f;
+
+    public Transform tran;
+    public Rigidbody rig;
+    public Animator ani;
+    public Rigidbody rigPick;
+
+
+    private void Update()
+    {
+        Turn();
+        Run();
+        Attack();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.name == "寶箱"&& ani.GetCurrentAnimatorStateInfo(0).IsName("攻擊"))
+        {
+            Physics.IgnoreCollision(other, GetComponent<Collider>());
+            other.GetComponent<HingeJoint>().connectedBody = rigPick;
+        }
+
+        if (other.name == "區域" && ani.GetCurrentAnimatorStateInfo(0).IsName("攻擊"))
+        {
+            GameObject.Find("寶箱").GetComponent<HingeJoint>().connectedBody = null;
+        }
+    }
+
+    private void Run()
+    {
+        if (ani.GetCurrentAnimatorStateInfo(0).IsName("攻擊")) return;
+
+        float v = Input.GetAxis("Vertical"); // W S
+        rig.AddForce(tran.forward * speed * v * Time.deltaTime);
+
+        ani.SetBool("走路開關", v != 0);
+
+    }
+
+    private void Turn()
+    {
+        float h = Input.GetAxis("Horizontal"); // A D
+        tran.Rotate(0, turn * h * Time.deltaTime , 0);
+
+    }
+
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ani.SetTrigger("攻擊觸發器");
+        }
+    }
+}
